78cfd0fa066718a43ead9153ed99934efd7e6a84
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2025-03-21 01:29:10 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      'e7b3-93-86-204-206.ngrok-free.app'
-    ]
-  }
+  base: process.env.VITE_BASE_PATH || "/Chiropractic-App",
 })
